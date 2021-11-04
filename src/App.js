f8dbafc2841c71efa1eb35fc1d0edaaadd34f830
jinwook-song <wlsdnr129@naver.com>
2021-11-04 23:32:35 +0900

    lecture 2. 1 styled component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const Box1 = styled.div`
+  background-color: teal;
+  width: 100px;
+  height: 100px;
+`;
+const Box2 = styled.div`
+  background-color: tomato;
+  width: 100px;
+  height: 100px;
+`;
+const Text = styled.span`
+  color: white;
+`;
+
 function App() {
-  return null;
+  return (
+    <Father>
+      <Box1>
+        <Text>Hello</Text>
+      </Box1>
+      <Box2 />
+    </Father>
+  );
 }
 
 export default App;
