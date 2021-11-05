ef5ee2de8f1aa9e20085c29db09246481c7a16cd
jinwook-song <wlsdnr129@naver.com>
2021-11-05 18:42:01 +0900

    lecture 2.7 Themes for darkmode

--- src/App.js
+++ src/App.js
@@ -1,61 +1,23 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 const Wrapper = styled.div`
   height: 100vh;
   width: 100vw;
-  background-color: teal;
+  background-color: ${(props) => props.theme.backgroundColor};
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
 
-const animation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius:0;
-  }
-  50% {
-    transform: rotate(360deg);
-    border-radius:100px;
-  }
-  100% {
-    transform: rotate(0deg);
-    border-radius:0;
-  }
-`;
-
-const Emoji = styled.span`
-  will-change: contents;
-  font-size: 3rem;
-`;
-
-const Box = styled.div`
-  height: 10rem;
-  width: 160px;
-  background-color: black;
-  animation: ${animation} 2s ease-in-out infinite;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  &:hover {
-    animation: none;
-    border-radius: 10%;
-  }
-  ${Emoji} {
-    transition: 1s;
-    &:hover {
-      font-size: 6rem;
-    }
-  }
-`;
-
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji>🔥</Emoji>
-      </Box>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
