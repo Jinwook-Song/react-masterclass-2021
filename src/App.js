9db2c01ffe9387a9f7fa51a12a9634c9c19ffbf6
jinwook-song <wlsdnr129@naver.com>
2021-11-05 16:56:49 +0900

    lecture 2.3 'As' and Attrs

--- src/App.js
+++ src/App.js
@@ -2,23 +2,34 @@ import styled from "styled-components";
 
 const Father = styled.div`
   display: flex;
+  flex-direction: column;
 `;
 
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
+  padding: 10px 20px;
 `;
 
-const Circle = styled(Box)`
-  border-radius: 50%;
+const Input = styled.input.attrs({ required: true, minLength: 10 })`
+  background-color: pink;
 `;
 
 function App() {
   return (
-    <Father>
-      <Box bgColor="teal" />
-      <Circle bgColor="tomato" />
+    <Father as="header">
+      <Btn>Log in</Btn>
+      <Btn as="a" href="/">
+        Log in
+      </Btn>
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </Father>
   );
 }
