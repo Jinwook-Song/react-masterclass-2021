95441259f1fec526a286c0e0fdcd070a39636267
jinwook-song <wlsdnr129@naver.com>
2021-11-06 19:57:01 +0900

    lecture 4.7 Nested Routes part 1

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router";
+import { Outlet, useLocation, useParams } from "react-router";
+import { Routes, Route, Link } from "react-router-dom";
+
 import styled from "styled-components";
+import Chart from "./Chart";
+import Price from "./Price";
 
 const Container = styled.div`
   padding: 0 1rem;
-  max-width: 480px;
+  max-width: 500px;
   margin: 0 auto;
 `;
 
@@ -28,12 +32,36 @@ const Title = styled.h1`
   color: ${(props) => props.theme.accentColor};
 `;
 
-interface ITag {
-  coin_counter: number;
-  ico_counter: number;
-  id: string;
-  name: string;
-}
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 0.5rem 1rem;
+  border-radius: 0.5rem;
+`;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  span {
+    font-size: max(min(1.5rem, 20px), 15px);
+    color: ${(props) => props.theme.accentColor};
+  }
+  span:first-child {
+    font-size: max(min(1rem, 15px), 12px);
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 0.5rem;
+    color: ${(props) => props.theme.accentColor2};
+  }
+`;
+
+const Description = styled.p`
+  margin: 1rem 0;
+  font-size: max(min(1.5rem, 20px), 15px);
+  color: ${(props) => props.theme.accentColor};
+`;
 
 interface InfoData {
   id: string;
@@ -109,13 +137,46 @@ function Coin() {
       setPrice(priceData);
       setLoading(false);
     })();
-  }, []);
+  }, [coinId]);
   return (
     <Container>
       <Header>
-        <Title>{state?.name ?? null}</Title>
+        <Title>
+          {state?.name ? state.name : loading ? "Loading..." : info?.name}
+        </Title>
       </Header>
-      {loading ? <Loader>Loading...</Loader> : null}
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Overview>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{info?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol:</span>
+              <span>{info?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Open Source:</span>
+              <span>{info?.open_source}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>{info?.description}</Description>
+          <Overview>
+            <OverviewItem>
+              <span>Total Supply:</span>
+              <span>{price?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Max Supply:</span>
+              <span>{price?.max_supply}</span>
+            </OverviewItem>
+          </Overview>
+          <Outlet />
+        </>
+      )}
     </Container>
   );
 }
---
+++ src/routes/Price.tsx
@@ -0,0 +1,5 @@
+function Price() {
+  return <h1>Price Screen</h1>;
+}
+
+export default Price;
