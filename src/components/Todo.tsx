94740c90d68c705b8fdc48f0a0528e3765394a22
jinwook-song <wlsdnr129@naver.com>
2021-11-09 19:48:15 +0900

    lecture 5.13 Categories

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,12 +1,38 @@
-import { ITodo } from "../atoms";
+import React from "react";
+import { useSetRecoilState } from "recoil";
+import { ITodo, todoState } from "../atoms";
 
-function Todo({ text }: ITodo) {
+function Todo({ id, text, category }: ITodo) {
+  const setTodos = useSetRecoilState(todoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name: categoryState },
+    } = event;
+  };
   return (
     <li>
       {text}
-      <button>To Do</button>
-      <button>Doing</button>
-      <button>Done</button>
+      <button
+        name="TODO"
+        onClick={onClick}
+        disabled={category === "TODO" ? true : false}
+      >
+        To Do
+      </button>
+      <button
+        name="DOING"
+        onClick={onClick}
+        disabled={category === "DOING" ? true : false}
+      >
+        Doing
+      </button>
+      <button
+        name="DONE"
+        onClick={onClick}
+        disabled={category === "DONE" ? true : false}
+      >
+        Done
+      </button>
     </li>
   );
 }
