fef47d1170e38f540a96645882417a1848f9cf4a
jinwook-song <wlsdnr129@naver.com>
2021-11-12 21:19:39 +0900

    lecture 7.9 useViewportScroll and motion values

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import styled from "styled-components";
-import { motion, useMotionValue, useTransform, Variants } from "framer-motion";
+import {
+  motion,
+  useMotionValue,
+  useTransform,
+  useViewportScroll,
+} from "framer-motion";
 
-const Wrapper = styled.div`
-  height: 100vh;
+const Wrapper = styled(motion.div)`
+  height: 200vh;
   width: 100vw;
   display: flex;
   justify-content: center;
@@ -17,24 +22,26 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants: Variants = {
-  hover: { scale: 1.2, rotateZ: 90 },
-  click: { scale: 0.8, borderRadius: 100 },
-  drag: {
-    backgroundColor: "#00ff00",
-    transition: { duration: 0.5 },
-  },
-};
-
 function App() {
+  // mouse move
   const x = useMotionValue(0);
-  const input = [-400, 0, 400];
-  const output = [2, 1, 0.2];
-  const scale = useTransform(x, input, output);
+  const rotateZ = useTransform(x, [-600, 600], [-360, 360]);
+  const gradient = useTransform(
+    x,
+    [-600, 0, 600],
+    [
+      "linear-gradient(135deg, rgb(58, 225, 255), rgb(69, 81, 250))",
+      "linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238))",
+      "linear-gradient(135deg, rgb(138, 252, 63), rgb(55, 185, 22))",
+    ]
+  );
 
+  // scroll move
+  const { scrollYProgress } = useViewportScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 0.2]);
   return (
-    <Wrapper>
-      <Box style={{ x, scale }} drag="x" dragSnapToOrigin />
+    <Wrapper style={{ background: gradient }}>
+      <Box style={{ x, rotateZ, scale }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
