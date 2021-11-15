8bca257c388365b98e86ae0bc9c016ffd9da4b04
jinwook-song <wlsdnr129@naver.com>
2021-11-15 20:13:52 +0900

    lecture 8.2 Header animation

--- src/Routes/Components/Header.tsx
+++ src/Routes/Components/Header.tsx
@@ -1,5 +1,6 @@
+import { Link, useMatch } from "react-router-dom";
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 
 const Nav = styled.nav`
   display: flex;
@@ -11,7 +12,7 @@ const Nav = styled.nav`
   background-color: black;
   color: white;
   font-size: max(0.5rem, 16px);
-  padding: 0.6rem 3rem;
+  padding: 0.6rem 2.5rem;
 `;
 
 const Col = styled.div`
@@ -25,8 +26,8 @@ const Logo = styled(motion.svg)`
   height: 1.2rem;
   fill: ${(props) => props.theme.red};
   path {
-    stroke-width: 0.1rem;
-    stroke: white;
+    stroke-width: 0.3rem;
+    stroke: ${(props) => props.theme.red};
   }
 `;
 
@@ -39,9 +40,27 @@ const Item = styled.li`
   margin-right: 0.8rem;
   color: ${(props) => props.theme.white.darkWhite};
   transition: color 0.3 ease-in-out;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
   &::hover {
     color: ${(props) => props.theme.white.lightWhite};
   }
+`;
+
+const Circle = styled.span`
+  position: absolute;
+  width: 0.3rem;
+  height: 0.3rem;
+  max-width: 5px;
+  max-height: 5px;
+  background-color: ${(props) => props.theme.red};
+  border-radius: 50%;
+  bottom: -0.4rem;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
 `;
 
 const Search = styled.span`
@@ -51,11 +70,30 @@ const Search = styled.span`
   }
 `;
 
+// for animation
+const logoVariants: Variants = {
+  normal: {
+    fillOpacity: 1,
+  },
+  active: {
+    fillOpacity: [1, 0, 1],
+    transition: {
+      repeat: 2,
+    },
+  },
+};
+
 function Header() {
+  const isHome = useMatch("/");
+  const isTv = useMatch("/tv");
+  console.log(isHome, isTv);
   return (
     <Nav>
       <Col>
         <Logo
+          variants={logoVariants}
+          initial="normal"
+          whileHover="active"
           xmlns="http://www.w3.org/2000/svg"
           width="1024"
           height="276.742"
@@ -64,8 +102,12 @@ function Header() {
           <motion.path d="M140.803 258.904c-15.404 2.705-31.079 3.516-47.294 5.676l-49.458-144.856v151.073c-15.404 1.621-29.457 3.783-44.051 5.945v-276.742h41.08l56.212 157.021v-157.021h43.511v258.904zm85.131-157.558c16.757 0 42.431-.811 57.835-.811v43.24c-19.189 0-41.619 0-57.835.811v64.322c25.405-1.621 50.809-3.785 76.482-4.596v41.617l-119.724 9.461v-255.39h119.724v43.241h-76.482v58.105zm237.284-58.104h-44.862v198.908c-14.594 0-29.188 0-43.239.539v-199.447h-44.862v-43.242h132.965l-.002 43.242zm70.266 55.132h59.187v43.24h-59.187v98.104h-42.433v-239.718h120.808v43.241h-78.375v55.133zm148.641 103.507c24.594.539 49.456 2.434 73.51 3.783v42.701c-38.646-2.434-77.293-4.863-116.75-5.676v-242.689h43.24v201.881zm109.994 49.457c13.783.812 28.377 1.623 42.43 3.242v-254.58h-42.43v251.338zm231.881-251.338l-54.863 131.615 54.863 145.127c-16.217-2.162-32.432-5.135-48.648-7.838l-31.078-79.994-31.617 73.51c-15.678-2.705-30.812-3.516-46.484-5.678l55.672-126.75-50.269-129.992h46.482l28.377 72.699 30.27-72.699h47.295z" />
         </Logo>
         <Items>
-          <Item>Home</Item>
-          <Item>Tv Shows</Item>
+          <Item>
+            <Link to="/">Home {isHome ? <Circle /> : null}</Link>
+          </Item>
+          <Item>
+            <Link to="/tv">TV {isTv ? <Circle /> : null}</Link>
+          </Item>
         </Items>
       </Col>
       <Col>
